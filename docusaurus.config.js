f229f17104c1dc574ff22ae690e1da757f6da326
ilioner <349329886@qq.com>
2023-03-01 22:05:44 +0800

    no message

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Midjourney中文社区',
+  tagline: 'AI，Midjourney，Prompt',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -48,8 +48,8 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Midjourney中文社区',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
